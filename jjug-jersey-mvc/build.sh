56730c08f0a32f84305414dd10f5c4b02dc892c3
MasatoshiTada <rosso.e.nero84@gmail.com>
2016-05-08 11:07:32 +0900

    Uber JAR用の起動シェルスクリプトを追加

---
+++ jjug-jersey-mvc/build.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+export GF_HOME=~/Java/ap-server/payara-web-ml-161.1
+mvn clean package
+$GF_HOME/bin/asadmin start-database
+$GF_HOME/bin/asadmin start-domain domain1
+$GF_HOME/bin/asadmin undeploy jjug-jersey-mvc
+$GF_HOME/bin/asadmin deploy --contextroot=jjug-jersey-mvc --name=jjug-jersey-mvc target/jjug-jersey-mvc-1.0-SNAPSHOT.war
+open -a Safari http://localhost:8080/jjug-jersey-mvc/
+tail -f $GF_HOME/glassfish/domains/domain1/logs/server.log
---
+++ jjug-mvc10/build.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+export GF_HOME=~/Java/ap-server/payara-web-ml-161.1
+mvn clean package
+$GF_HOME/bin/asadmin start-database
+$GF_HOME/bin/asadmin start-domain domain1
+$GF_HOME/bin/asadmin undeploy jjug-mvc10
+$GF_HOME/bin/asadmin deploy --contextroot=jjug-mvc10 --name=jjug-mvc10 target/jjug-mvc10-1.0-SNAPSHOT.war
+open -a Safari http://localhost:8080/jjug-mvc10/
+tail -f $GF_HOME/glassfish/domains/domain1/logs/server.log
