d2f1f11944934c5f73ee79108ada8b391b9d21b8
MasatoshiTada <rosso.e.nero84@gmail.com>
2016-05-08 13:27:15 +0900

    起動用シェルスクリプトを追加

---
+++ jjug-resteasy-html/build.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+export AS_HOME=~/Java/ap-server/wildfly-10.0.0.Final/
+mvn clean package
+rm -rf $AS_HOME/standalone/deployments/jjug-*
+cp target/jjug-resteasy-html-1.0-SNAPSHOT.war $AS_HOME/standalone/deployments
+open -a Safari http://localhost:8080/jjug-resteasy-html-1.0-SNAPSHOT/
+$AS_HOME/bin/standalone.sh
